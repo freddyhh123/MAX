6b3f161fd44dbd4667c25ea352c6dfe2cda6355d
Fred Howard <fredhorvathoward@gmail.com>
2023-11-07 00:40:09 +0000

    More processing from spotify api

--- apicall.py
+++ apicall.py
@@ -1,18 +1,14 @@
 import spotipy
 import pandas as pd
-import requests
-from io import BytesIO
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import torch
+import torchaudio
+
+import urllib.request
 from pydub import AudioSegment
-import matplotlib.pyplot as pyplt
-from scipy.io import wavfile
-from scipy import signal
-from tempfile import mktemp
 
-
-
-def getTracks(sp):
+def getTracks():
     cid = "516f5defb70e4e0994acea4cc99d1d4c"
     secret = "0038b1fca65d467dbd0d2e1437041545"
 
@@ -24,15 +20,54 @@ def getTracks(sp):
     results = sp.playlist_items(playlist_uri)
     trackWithAudio = []
 
-    for entry in results['items'][:100]:
+    for entry in results['items'][:250]:
         track = entry["track"]
         if track['preview_url']:
-            track['genres'] = []
-            track['features'] = sp.audio_features(track["id"])[0]
-            for artist in track['artists']:
-                track["genres"] = sp.artist(artist["id"])["genres"]
-            trackWithAudio.append(entry)
+            compiledTrack = {}
+            compiledArtist = {}
+            compiledTrack['album'] = {}
+            compiledTrack['artists'] = []
+            for key, artist in enumerate(track['artists']):
+                compiledTrack["genres"] = sp.artist(artist["id"])["genres"]
+
+                compiledArtist["artist_url"] = artist['external_urls']['spotify']
+                compiledArtist["name"] = artist['name']
+                compiledArtist["id"] = artist['id']
+                compiledTrack["artists"].append(compiledArtist)
+
+            compiledTrack['name'] = track['name']
+            compiledTrack['id'] = track['id']
+            compiledTrack['popularity'] = track['popularity']
+            compiledTrack['preview_url'] = track['preview_url']
+            compiledTrack['spotify_url'] = track['external_urls']['spotify']
+            compiledTrack['spotify_id'] = track['uri']
+            compiledTrack['features'] = sp.audio_features(track["id"])[0]
+
+            compiledTrack['explicit'] = track['explicit']
+            
+            compiledTrack['album']['name'] = track['album']['name']
+            compiledTrack['album']['id'] = track['album']['id']
+            compiledTrack['album']['type'] = track['album']['album_type']
+            compiledTrack['album']['url'] = track['album']['external_urls']['spotify']
+            compiledTrack['album']['image'] = track['album']['images']
+            compiledTrack['album']['release_date'] = track['album']['release_date']
+
+            urllib.request.urlretrieve(compiledTrack['preview_url'], "audio/" + str(compiledTrack['id']) + ".mp3")
+            wav = AudioSegment.from_mp3("audio/" + str(compiledTrack['id']) + ".mp3")
+            
+            mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+                sample_rate=SAMPLE_RATE,
+                n_fft=1024,
+                hop_length=512,
+                n_mels=64
+            )
+            1==1
+
+            trackWithAudio.append(compiledTrack)
     return trackWithAudio
+
+tracks = getTracks()
+1 == 1
 
             
 
