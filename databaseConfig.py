841f0f18b3fd2f65160515dc1692c254cb838842
unknown <fredhorvathoward@gmail.com>
2024-01-14 16:33:02 +0000

    modified database config to check for platform

--- databaseConfig.py
+++ databaseConfig.py
@@ -1,9 +1,19 @@
 import mysql.connector
+from sys import platform
 
 def connect():
-    return mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="max"
-)
+    if platform == "linux" or platform == "linux2":
+        return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="max",
+        auth_plugin='mysql_native_password'
+        )
+    elif platform == "win32" or platform == "win64":
+        return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="max"
+        ) 
