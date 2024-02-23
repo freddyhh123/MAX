090101c7d08e829c558c7fb621d1e672273f0d68
unknown <fredhorvathoward@gmail.com>
2024-02-23 15:38:09 +0000

    re-do genre classifier

--- train_genre_classifier.py
+++ train_genre_classifier.py
@@ -1,92 +1,159 @@
-import prepareDataset
-from fmaDataset import fmaDataset
-import torch.optim as optim
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader
-import torch.optim as optim
-import torch.nn as nn
-from genreModel import topGenreClassifier
-from genreModel import train_model
-import torch.cuda
-from torch.nn.functional import pad
+# Standard python libs
 import os
+# Third-party libs
+import numpy as np
 import pandas as pd
-import pickle
+from sklearn.metrics import accuracy_score, f1_score, hamming_loss
+from sklearn.model_selection import train_test_split
+# PyTorch libs
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.functional import pad
+from torch.utils.data import DataLoader
+# Local libs
+from fmaDataset import fmaDataset
+from genreModel import topGenreClassifier, train_model
+from prepareDataset import buildDataframe
 
-
+# This is our collate function, it allows the loader to process the features
+# by making them the right "shape" for the model.
 def resize_collate(batch):
-    processed_features = []
-    labels = []
+    processed_features = [pad(features, (0, 2580 - features.shape[2]), "constant", 0) if features.shape[2] < 2580 else features[:, :, :2580] for features, label in batch]
+    labels = torch.stack([torch.tensor(label) for _, label in batch])
+    return torch.stack(processed_features), labels
 
-    for features, label in batch:
-        # Handle features
-        current_size = features.shape[2]
-        if current_size < 2580:
-            # Pad the features tensor
-            padding_needed = 2580 - current_size
-            features = pad(features, (0, padding_needed), "constant", 0)
-        elif current_size > 2580:
-            # Trim the features tensor
-            features = features[:, :, :2580]
-        elif features.shape[0] == 1:
-            features = features.repeat(2, 1, 1)
-        processed_features.append(features)
+# Loads a checkpoint for training
+def load_ckp(checkpoint_fpath, model, optimizer):
+    checkpoint = torch.load(checkpoint_fpath)
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    return model, optimizer
 
-        # Handle labels
-        labels.append(torch.tensor(label))
-        #labels.append(label)
+# This checks whether a file exists then writes appends to it.
+def append_or_create(results_dataframe, file_path):
+    if os.path.exists(file_path):
+        existing_dataframe = pd.read_csv(file_path)
+        updated_dataframe = pd.concat([existing_dataframe, results_dataframe], ignore_index=True)
+    else:
+        updated_dataframe = results_dataframe
 
-    labels = torch.stack(labels)
-    features_batch = torch.stack(processed_features)
+    updated_dataframe.to_csv(file_path, index=False)
 
-    return features_batch, labels
+# This calculates our genre metrics and outputs them ready for file export
+def calculate_metrics(metrics, labels_predictions, train_results, val_results, epoch):
+    # Converting our labels to a format readable by sklearn etc.
+    labels = torch.cat(labels_predictions['labels'], dim=0).numpy()
+    predictions = torch.cat(labels_predictions['predictions'], dim=0).numpy()
+    val_all_labels = np.concatenate(labels)
+    val_all_predictions = np.concatenate(predictions)
 
-if not os.path.exists("tracks.pkl"):
-    track_dataframe = prepareDataset.buildDataframe("genre", False)
-    with open('tracks.pkl', 'wb') as file:
-        pickle.dump(track_dataframe, file)
-else:
-    with open('tracks.pkl', 'rb') as file:
-        track_dataframe = pickle.load(file)
+    metrics['epoch'].append(epoch + 1)
+    metrics['validation_accuracy'].append(sum(val_results['validation_accuracy']) / len(val_results['validation_accuracy']))
+    metrics['train_accuracy'].append(sum(train_results['train_accuracy']) / len(train_results['train_accuracy']))
+    metrics['validation_loss'].append(sum(val_results['validation_loss']) / len(val_results['validation_loss']))
+    metrics['train_loss'].append(sum(train_results['train_loss']) / len(train_results['train_loss']))
+    # Calculate our metrics based off labels and predictions
+    metrics['f1_macro'].append(f1_score(val_all_labels, val_all_predictions, average='macro'))
+    metrics['f1_micro'].append(f1_score(val_all_labels, val_all_predictions, average='micro'))
+    metrics['hamming_loss'].append(hamming_loss(val_all_labels, val_all_predictions))
+    metrics['subset_accuracy'].append(accuracy_score(val_all_labels, val_all_predictions))
+    return metrics
 
 
-rows_with_na = track_dataframe.isna().any(axis=1)
-na_rows_exist = rows_with_na.any()
-if na_rows_exist:
-    rows_with_na_df = track_dataframe[rows_with_na]
-    print(rows_with_na_df)
+def main():
+    # If the data isnt ready i.e. you don't have the files, uncomment
+    #buildDataframe("genres", True)
+    folder_path = 'track_genre_files'
+    files = [file for file in os.listdir(folder_path) if file.endswith(".pkl")]
 
-track_dataframe.reset_index(drop=True, inplace=True)
+    # Setting our main hyperparameters
+    batch_sizes = [10,25,50]
+    learning_rates = [0.0001,0.001,0.01,0.1,0.25]
+    epoch_sizes = [10,25,50,100,250,500]
 
-dataset = fmaDataset(dataframe = track_dataframe, spectrogram = track_dataframe['spectrogram'].values, mfcc = track_dataframe['mfcc'], labels = track_dataframe['genre_vector'], id = track_dataframe['track_id'])
+    for epoch_size in epoch_sizes:
+        for batch_size in batch_sizes:
+            for learning_rate in learning_rates:
+                results_file = str(epoch_size)+"-"+str(batch_size)+"-"+str(learning_rate)+"-genre"+"_results.csv"
+                print("epoch_size : "+str(epoch_size)+"-batch_size: "+str(batch_size)+"-learning_rate: "+str(learning_rate))
+                # Initialise the model and find whether we have CUDA available
+                # TODO, ensure non cuda support
+                model = topGenreClassifier()
+                optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+                criterion = nn.BCEWithLogitsLoss()
+                device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                model.to(device)
 
-batch_sizes = [1,10,50,100,250,500]
-learning_rates = [0.0001,0.001,0.01,0.1,0.25]
-num_epochs = [25,50,100,250,500,1000]
-results = []
+                # If we need to use checkpoints e.g. process was stopped, uncomment
+                #if os.path.exists("max_genre_v1.pth"):
+                    #model.load_state_dict(torch.load("max_genre_v1.pth"))
 
-for epoch in num_epochs:
-    for batch_size in batch_sizes:
-        for learning_rate in learning_rates:
-            train_df, test_df = train_test_split(dataset, test_size=0.2, train_size=0.8, random_state=666)
-            #test_df, validation_df = train_test_split(test_df, test_size=0.5, train_size=0.5, random_state=666)
+                #if os.path.exists("max_genre_checkpoint.pt"):
+                    #model, optimizer = load_ckp("max_genre_checkpoint.pt", model, optimizer)
+                
+                for epoch in range(epoch_size):
+                    # Set up all our storage variables
+                    metrics = {'epoch': [], 'f1_macro': [], 'f1_micro': [], 'hamming_loss': [], 'subset_accuracy': [], 'validation_accuracy': [], 'train_accuracy': [], 'validation_loss':[], 'train_loss':[]}
+                    epoch_val_results = {
+                        'validation_accuracy' : list(),
+                        'validation_loss' : list()
+                    }
+                    epoch_train_results = {
+                        'train_accuracy' : list(),
+                        'train_loss' : list()
+                    }
+                    epoch_labels_predictions = {
+                        'labels' : [],
+                        'predictions' : []
+                    }
+                    print("Epoch: "+str(epoch + 1)+"/"+str(epoch_size))
+                    for idx, file_name in enumerate(files):
 
-            batch_size = batch_size
-            train_loader = DataLoader(train_df, batch_size = batch_size, collate_fn = resize_collate, shuffle=True)
-            test_loader = DataLoader(test_df, batch_size = batch_size, collate_fn = resize_collate, shuffle=False)
+                        name, extension = os.path.splitext(file_name)
+                        if extension != ".pkl":
+                            continue
+                        
+                        # Read the file we are working on
+                        track_dataframe = pd.read_pickle(os.path.join(folder_path, file_name))
+                        track_dataframe.reset_index(drop=True, inplace=True)
 
-            model = topGenreClassifier()
-            optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-            criterion = nn.BCEWithLogitsLoss()
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            model.to(device)
+                        # Load the dataset and split into loaders
+                        dataset = fmaDataset(dataframe = track_dataframe, spectrogram = track_dataframe['spectrogram'].values, mfcc = track_dataframe['mfcc'], labels = track_dataframe['genre_vector'], id = track_dataframe['track_id'])
+                        train_df, test_df = train_test_split(dataset, test_size=0.2, train_size=0.8, random_state=666)
+                        
+                        train_loader = DataLoader(train_df, batch_size = batch_size, collate_fn = resize_collate, shuffle=True)
+                        test_loader = DataLoader(test_df, batch_size = batch_size, collate_fn = resize_collate, shuffle=False)
 
-            train_results, val_results = train_model(model, train_loader, test_loader, criterion, optimizer, epoch)
+                        print("Analysing file: " + str(file_name) + "File no: "+ str(idx + 1) +"/"+ str(len(files)))
+                        print("Tracks to analyse: "+str(len(track_dataframe.index)+1))
+                        # Train the model with this file's tracks
+                        train_results, val_results, labels_predictions = train_model(model, train_loader, test_loader, criterion, optimizer, epoch, device)
+                        
+                        # There is some odd formatting with dictionaries, so this is here just to make sure
+                        # the format is right for metric calculations
+                        # TODO maybe look at re-doing the formats here and in the model code
+                        for label, prediction in zip(labels_predictions['labels'], labels_predictions['predictions']):
+                            epoch_labels_predictions['labels'].append(label)
+                            epoch_labels_predictions['predictions'].append(prediction)
 
-            train_results_dataframe = pd.DataFrame(train_results)
-            val_results_dataframe = pd.DataFrame(val_results)
-            train_results_dataframe.to_csv(str(epoch)+"-"+str(batch_size)+"-"+str(learning_rate)+"-"+'train_results.csv')
-            val_results_dataframe.to_csv(str(epoch)+"-"+str(batch_size)+"-"+str(learning_rate)+"-"+'val_results.csv')
-            print("Finished Learning rate: " + str(learning_rate))
-        print("Finished batch_size: "+ str(batch_size))
-    print("Finished epoch: "+ str(epoch))
+                        epoch_val_results['validation_accuracy'].append(val_results['val_accuracy'])
+                        epoch_train_results['train_accuracy'].append(train_results['train_accuracy'])
+
+                        epoch_train_results['train_loss'].append(train_results['train_loss'])
+                        epoch_val_results['validation_loss'].append(val_results['val_loss'])
+                    
+                    metrics = calculate_metrics(metrics, epoch_labels_predictions, epoch_train_results, epoch_val_results, epoch)
+                    append_or_create(pd.DataFrame(metrics), results_file)
+
+                    # Save our progress just in case!
+                    torch.save({
+                        'epoch' : epoch + 1,
+                        'state_dict': model.state_dict(),
+                        'optimizer': optimizer.state_dict(),
+                    }, "max_genre_checkpoint.pt")
+
+            torch.save(model.state_dict(), "max_genre_v3.pth")
+
+if __name__ == "__main__":
+    main()
