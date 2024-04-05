3fd161c7f96609da9ce6a4bb640b46c317c9bc76
unknown <fredhorvathoward@gmail.com>
2024-04-05 15:56:22 +0100

    updated utils file

--- maxUtils.py
+++ maxUtils.py
@@ -5,6 +5,8 @@ from annoy import AnnoyIndex
 from databaseConnector import connect
 import csv
 import json
+import torchaudio
+from subGenreModel import SubGenreClassifier
 
 # Loads a checkpoint for training
 def load_ckp(checkpoint_fpath, model, optimizer):
@@ -75,4 +77,46 @@ def update_ann():
     ann_index.build(10)
     ann_index.save('ANN_tracks_index.ann')
 
-update_averages()
+def check_bad_files():
+    directory = 'data'
+    bad_files = []
+    fields = ['folder', 'file', 'error'] 
+
+    for folder in os.listdir(directory):
+        if folder == "checksums":
+            continue
+        for filename in os.listdir(os.path.join(directory, folder)):
+            if filename.endswith('.mp3'):
+                full_path = os.path.join(directory, folder, filename)
+                try:
+                    torchaudio.load(full_path, normalize=True)
+                except Exception as e:  # Catch the exception to understand the issue
+                    bad_files.append((folder, filename, str(e)))
+
+    with open('bad_files.csv', 'w',newline='') as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(bad_files)
+
+# This gets the epoch_size, batch_size and the learning_rate from the filename
+def extract_parameters_from_filename(filename):
+    parts = filename.split('_')
+    params = parts[0].split('-')
+    epoch_size, batch_size, learning_rate = params[:3]
+    return epoch_size, batch_size, learning_rate
+
+def initialize_sub_genre_models():
+    # info for generating sub-genre models, left side is genre_id
+    # right side is the number of sub_genres or outputs
+    sub_genre_models = {}
+    sub_genre_info = [30,2,7,8,1,6,3,29,4,4,19,6,12,8,19,5]
+    for idx, sub_genres in enumerate(sub_genre_info):
+                    sub_model = SubGenreClassifier(num_classes = sub_genres)
+                    sub_genre_models[idx] = sub_model
+    return sub_genre_models
+
+def get_top_to_sub_genre_map():
+    db = connect()
+    genre_relationship_query = "SELECT genre_id, top_genre FROM genres"
+    genre_relationships_df = pd.read_sql(genre_relationship_query, db)
+    return {row['genre_id']: row['top_genre'] for index, row in genre_relationships_df.iterrows()}
